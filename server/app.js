bf1d31480b0fe3d0cce4348a66db392cc6e485d9
Sergio Lopez <serlopsal_199@yahoo.es>
2019-04-26 08:35:47 -0600

    change path main-layout

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ const logger = winston.createLogger({
 //View Config
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine', 'ejs');
-app.set('layout', 'layouts/main-layout');
+app.set('layout', path.join(__dirname, '/views/layouts/main-layout'));
 app.use(ejsLayouts);
 
 console.log(app.get('env'));
