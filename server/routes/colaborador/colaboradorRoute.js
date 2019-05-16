c721d22761fadd5dd35abefbe5d290364776a76d
Sergio Lopez <serlopsal_199@yahoo.es>
2019-05-16 17:25:31 -0600

    colaboradorRoute

--- server/routes/colaborador/colaboradorRoute.js
+++ server/routes/colaborador/colaboradorRoute.js
@@ -1,10 +1,10 @@
 const express = require('Express');
 const errorHandler = require('../../middleware/errorHandler');
-const agregarColaboradorRoute = express.Route();
+const agregarColaboradorRoute = express.Router();
 
 module.exports = agregarColaboradorRoute;
 
 agregarColaboradorRoute
-    .get('/colaborador/agregar', errorHandler(async(req,res)=>{
-        return res.status(200).render('page/colaborador/agregar',{title: 'AppGolkii'})
-    }))
+.get('/agregar', errorHandler(async(req,res)=>{
+    return res.render('page/colaborador/agregar',{title: 'AppGolkii'})
+}))
--- server/routes/route.js
+++ server/routes/route.js
@@ -3,6 +3,7 @@ const mainRouter = require('./main/mainRoute');
 const errorRoute = require('./errors/404');
 const personaRoute = require('./persona/personaRoute');
 const loginRoute = require('./main/loginRoute');
+const colaboradorRoute = require('./colaborador/colaboradorRoute')
 
 const router = express.Router();
 
@@ -10,7 +11,7 @@ router.use('/',mainRouter);
 router.use('/error',errorRoute);
 router.use('/persona',personaRoute);
 router.use('/login',loginRoute);
-
+router.use('/colaborador',colaboradorRoute);
 module.exports = router;
 
 
