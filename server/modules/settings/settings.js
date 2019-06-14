202b124c3487677f9030fa4391bebfc9640eb234
Sergio Lopez <serlopsal_199@yahoo.es>
2019-06-14 08:33:45 -0600

    servers
    change servers to deploy

--- server/modules/settings/settings.js
+++ server/modules/settings/settings.js
@@ -1,7 +1,7 @@
 'use strict'
 const   
     coreApiUrl = "http://192.168.1.242:5000/api",
-    // mongoApiUrl = "http://192.168.1.227:3000/api",
+    mongoApiUrl = "http://192.168.1.243:8084/api",
     // coreApiUrl = "http://localhost:5000/api",
-    mongoApiUrl = "http://localhost:8084/api",
+    // mongoApiUrl = "http://localhost:8084/api",
     TimeOut = 60000; //Milisegundos equivalente a "1 minuto = 60 segundos"
